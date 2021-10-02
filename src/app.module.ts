a32f4b497ab24c68dc921b4421472aaca0e46437
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-10-01 21:38:14 -0300

    Configuração do projeto pra deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { SalasModule } from './modules/salas/salas.module';
 import { FilmesModule } from './modules/filmes/filmes.module';
 import { UsersModule } from './modules/users/users.module';
 import { IngressosModule } from './modules/ingressos/ingressos.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfigService } from './services/config/database-config-service.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), SalasModule, FilmesModule, UsersModule, IngressosModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    SalasModule, 
+    FilmesModule, 
+    UsersModule,
+    IngressosModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/services/config/database-config-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConfigService {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+      return {
+          type: 'postgres',
+          port: this.configService.get<number>('DB_PORT'),
+          host: this.configService.get<string>('DB_HOST'),
+          username: this.configService.get<string>('DB_USERNAME'),
+          password: this.configService.get<string>('DB_PASSWORD'),
+          database: this.configService.get<string>('DB_DATABASE'),
+          synchronize: true,
+          entities: [__dirname + '/../../**/*.entity.{js,ts}']
+      }
+  };
+}
